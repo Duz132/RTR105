1a060d6d4cf191da1c0694d5e50650eb061dcf18
user <derak722@inbox.lv>
2019-09-26 15:40:25 +0300

    2019-26-09-15:40

--- Darbi/test_datu_tipi_2.c
+++ Darbi/test_datu_tipi_2.c
@@ -2,12 +2,12 @@
 #include <limits.h>
 int main()
 {
-int a = 50000;
-int b = 1000000;
-int c = a * b; //kaads ir sagaidaams rezultaats?
+long long int a = 50000;
+long long int b = 1000000;
+long long int c = a * b; //kaads ir sagaidaams rezultaats?
 
-printf("int datu tipa izmers ir: %d baiti \n", sizeof(int));
+printf("int datu tipa izmers ir: %ld baiti \n", sizeof(int));
 printf("Apreekjinam a un b reizinaajumu :\n");
-printf("a = %d, b = %d \n", a, b);
-printf("c = a * b = %d * %d = %d \n", a,b,c); //rezultaats uz ekrana
+printf("a = %lld, b = %lld \n", a, b);
+printf("c = a * b = %lld * %lld = %lld \n", a,b,c); //rezultaats uz ekrana
 }

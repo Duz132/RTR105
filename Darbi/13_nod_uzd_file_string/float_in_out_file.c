c3601d8f6a3cf8a96cdac30dbb99ad37634900e4
Stanislav <derak722@inbox.lv>
2019-11-08 21:43:28 +0200

    2019_08_11_21:43

--- Darbi/13_nod_uzd_file_string/float_in_out_file.c
+++ Darbi/13_nod_uzd_file_string/float_in_out_file.c
@@ -1,41 +1,36 @@
 #include<stdio.h>
-#include<string.h>
 
 int main()
 {
-//float sk1, sk2, sk3, sk4, sk5, sk6, sk7, sk8, sk9, sk10;
+float sk1, sk2, sk3, sk4, sk5, sk6, sk7, sk8, sk9, sk10;
 char test[100];
-//FILE *fptr;
-int i;
-//fptr=fopen("float_test.txt","w");
+FILE *fptr;
+fptr=fopen("float_test.txt","w");
 
-printf("Cien. liet., uzraksti luudzu 10 reaalus skaitlus - ");
-//scanf("%f %f %f %f %f %f %f %f %f %f", &sk1, &sk2, &sk3, &sk4, &sk5, &sk6, &sk7, &sk8, &sk9, &sk10);
-scanf("%[^\n]",&test);
-printf("TEST");
-for(i=0;i<strlen(test);i++)
-{
- printf("%s\n",test);
-}
+printf("Cien. liet., uzraksti luudzu 10 reaalus skaitlus - "); //PIEMERS: 12.321 45.345 32.34 45.763 64.233 87.76 56.4 34.56 24.567 82.664
+//scanf("%f%f%f%f%f%f%f%f%f%f", &sk1, &sk2, &sk3, &sk4, &sk5, &sk6, &sk7, &sk8, &sk9, &sk10);
+scanf("%[^\n]", &test);
+fprintf(fptr,"%f   %f\n",sk1, sk2);
+fprintf(fptr,"%f   %f\n",sk3, sk4);
+fprintf(fptr,"%f   %f\n",sk5, sk6);
+fprintf(fptr,"%f   %f\n",sk7, sk8);
+fprintf(fptr,"%f   %f\n",sk9, sk10);
 
-//fprintf(fptr,"%f   %f\n",sk1, sk2);
-//fprintf(fptr,"%f   %f\n",sk3, sk4);
-//fprintf(fptr,"%f   %f\n",sk5, sk6);
-//fprintf(fptr,"%f   %f\n",sk7, sk8);
-//fprintf(fptr,"%f   %f\n",sk9, sk10);
-//fclose(fptr);
+printf("TESTS\n");
+printf("%s\n",test);
+fclose(fptr);
 
-//fptr=fopen("float_test.txt","r");
+fptr=fopen("float_test.txt","r");
 
-//printf("FAILA SATURS\n");
-//while(fscanf(fptr,"%f%f%f%f%f%f%f%f%f%f", &sk1, &sk2, &sk3, &sk4, &sk5, &sk6, &sk7, &sk8, &sk9, &sk10) != EOF)
-//{
-//	printf("%f   %f   %f\n",sk1, sk2, sk1*sk2);
-//	printf("%f   %f   %f\n",sk3, sk4, sk3*sk4);
-//	printf("%f   %f   %f\n",sk5, sk6, sk5*sk6);
-//	printf("%f   %f   %f\n",sk7, sk8, sk7*sk8);
-//	printf("%f   %f   %f\n",sk9, sk10,sk9*sk10);
-//}
-//fclose(fptr);
+printf("Reaalie skaitli no faila + reizinaashanas rezuiltaats\n");
+fscanf(fptr,"%f%f%f%f%f%f%f%f%f%f", &sk1, &sk2, &sk3, &sk4, &sk5, &sk6, &sk7, &sk8, &sk9, &sk10);
+
+printf("%f   %f   %f\n",sk1, sk2, sk1*sk2);
+printf("%f   %f   %f\n",sk3, sk4, sk3*sk4);
+printf("%f   %f   %f\n",sk5, sk6, sk5*sk6);
+printf("%f   %f   %f\n",sk7, sk8, sk7*sk8);
+printf("%f   %f   %f\n",sk9, sk10,sk9*sk10);
+
+fclose(fptr);
 }
 
